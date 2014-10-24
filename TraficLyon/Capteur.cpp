6c878a82ef0a9ee21c09b20756d2b0a1c9a85530
Hugo <hugo.delval@insa-lyon.fr>
2014-10-24 14:38:56 +0200

    origin

---
+++ TraficLyon/Capteur.cpp
@@ -0,0 +1,18 @@
+/*
+ * Capteur.cpp
+ *
+ *  Created on: 24 oct. 2014
+ *      Author: Hugo
+ */
+
+#include "Capteur.h"
+
+Capteur::Capteur() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Capteur::~Capteur() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ TraficLyon/Capteur.h
@@ -0,0 +1,17 @@
+/*
+ * Capteur.h
+ *
+ *  Created on: 24 oct. 2014
+ *      Author: Hugo
+ */
+
+#ifndef CAPTEUR_H_
+#define CAPTEUR_H_
+
+class Capteur {
+public:
+	Capteur();
+	virtual ~Capteur();
+};
+
+#endif /* CAPTEUR_H_ */
