bc8672ad32de82224c6dfeaaab198444b047396b
Hugo <hugo.delval@insa-lyon.fr>
2014-10-28 13:32:18 +0100

    constructeur evenements

--- TraficLyon/Evenements.cpp
+++ TraficLyon/Evenements.cpp
@@ -4,14 +4,29 @@
  *  Created on: 24 oct. 2014
  *      Author: Hugo
  */
-
+#include <iostream>
+#include "ArbreIdentifiants.h"
+#include "Evenement.h"
 #include "Evenements.h"
+
+using namespace std;
 
 Evenements::Evenements() {
 
-	//ArbreIdentifiants arbreId = new ArbreIdentifiants;
-	evenements= new Evenement[1500][23][7][5];
-
+	ArbreIdentifiants arbreId;
+	evenements= new Evenement***[1500];
+	for(int i=0 ; i<1500 ; i++)
+	{
+		evenements[i] = new Evenement**[23];
+		for(int j=0 ; j<23 ; j++)
+		{
+			evenements[j] = new Evenement*[7];
+			for(int k=0 ; k<7 ; k++)
+			{
+				evenements[k] = new Evenement[5];
+			}
+		}
+	}
 }
 
 Evenements::~Evenements()
@@ -20,6 +35,6 @@ Evenements::~Evenements()
 	cout<< "Evenements::~Evenements()" <<endl;
 #endif
 	delete [] evenements;
-
+// a modifier : boucles comme dans le constructeur
 }
 
