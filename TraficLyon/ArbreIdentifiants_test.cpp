34991a11ddb9ab79ef320a802237e74ce8310c53
Hugo <hugo.delval@insa-lyon.fr>
2014-10-28 13:01:38 +0100

    ArbreIdentifiants_test

--- TraficLyon/ArbreIdentifiants_test.cpp
+++ TraficLyon/ArbreIdentifiants_test.cpp
@@ -4,6 +4,28 @@
  *  Created on: 24 oct. 2014
  *      Author: Hugo
  */
-
+#include <iostream>
 #include "ArbreIdentifiants.h"
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
 
+using namespace std;
+
+int main()
+{
+	/* initialize random seed: */
+	srand (time(NULL));
+
+	cout<<"GO!"<<endl;
+	ArbreIdentifiants a(0,50,50,0);
+	int iterations =10;
+	int idCourant=0;
+	a.afficher();
+	for (int i=0 ; i<iterations ; i++)
+	{
+		idCourant = (rand() % (500000))+1;
+		a.inserer(idCourant,i);
+		a.afficher();
+	}
+
+}
