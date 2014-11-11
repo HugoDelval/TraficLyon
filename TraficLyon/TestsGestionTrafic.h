4b1f4ba1cc87a098898ba2cc8271d0349dfffc81
Papin <alexis.papin@insa-lyon.fr>
2014-11-11 15:51:44 +0100

    Tests

---
+++ TraficLyon/TestsGestionTrafic.h
@@ -0,0 +1,30 @@
+/*************************************************************************
+              	  	  TestsGestionTrafic.cpp  -  Objet 'TestsGestionTrafic'
+                             -------------------
+    début                : 07/11/2014
+    copyright            : (C) 2014 par PAPIN/DELVAL
+*************************************************************************/
+
+
+//---------- Interface de la classe <Date.cpp> (fichier Date.h) ------
+
+#ifndef TESTSGESTIONTRAFIC_H_
+#define TESTSGESTIONTRAFIC_H_
+
+//--------------------------------------------------- Interfaces utilisées
+#include "GestionTrafic.h"
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Date>
+//
+//------------------------------------------------------------------------
+
+class TestsGestionTrafic {
+public:
+	TestsGestionTrafic();
+	virtual ~TestsGestionTrafic();
+};
+
+#endif /* TESTSGESTIONTRAFIC_H_ */
